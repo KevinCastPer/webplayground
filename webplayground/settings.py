f0236ef1c3b6d43593b0d8389086c9618235cd59
KevinCastPer <kecasper@gmail.com>
2019-02-18 18:36:31 +0100

    Creando un perfil de usuario

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Auth redirects
-LOGIN_REDIRECT_URL = 'pages:pages'
+# LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
 
 # Emails
@@ -132,4 +132,9 @@ if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
     EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 else:
+    # Aquí hay que configurar un email real para producción
     pass
+
+# Media Files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
